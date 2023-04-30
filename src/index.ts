8e7915b99606aa402af126a8b83098e6f1c30964
Amy <amy@litdevs.org>
2023-04-30 21:58:48 +0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import Reply from "./classes/Reply/Reply.js";
+import fs from "fs";
+
+const pjson = JSON.parse(fs.readFileSync("package.json").toString());
+const ejson = JSON.parse(fs.readFileSync("environment.json").toString());
+
+const app = express();
+
+app.get("/", async (req, res) => {
+    res.json(new Reply(
+        {
+            response: {
+                message: "OK",
+                version: pjson.version,
+                env: ejson
+            }
+        }));
+})
+
+app.listen(process.env.PORT || 13717, () => {
+    console.log("Listening on port 13717");
+})
